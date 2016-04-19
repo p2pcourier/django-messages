73712d60e6bf3de1c7f17104b8c28d31a854754b
Said B <phoenix49@gmail.com>
2016-04-19 13:57:27 +0100

    Revert "Merge to fix issues for Django 1.9"

--- django_messages/forms.py
+++ django_messages/forms.py
@@ -10,7 +10,6 @@ else:
 
 from django_messages.models import Message
 from django_messages.fields import CommaSeparatedUserField
-from django.core.exceptions import ValidationError
 
 class ComposeForm(forms.Form):
     """
@@ -30,8 +29,6 @@ class ComposeForm(forms.Form):
     
                 
     def save(self, sender, parent_msg=None):
-        if sender is None:
-            raise ValidationError(self.error_messages['empty_sender'], code='empty_sender')
         recipients = self.cleaned_data['recipient']
         subject = self.cleaned_data['subject']
         body = self.cleaned_data['body']
